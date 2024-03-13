440af22f414e703bf396d651f1213b3c3465cce3
FelipeFabricio <ff.oliveira32@gmail.com>
2024-03-13 13:26:47 -0300

    Altera configurações do backend

--- 1 - provider.tf
+++ 1 - provider.tf
@@ -5,13 +5,15 @@ terraform {
       version = "3.93.0"
     }
   }
+
   backend "azurerm" {
     resource_group_name  = "wdf-service-principal"
     storage_account_name = "wonderfoodstorage"
     container_name       = "tf-deploy"
-    key                  = var.azure_blob_storage_key
+    key                  = "terraform.tfstate"
   }
 }
+
 
 provider "azurerm" {
   features {}
