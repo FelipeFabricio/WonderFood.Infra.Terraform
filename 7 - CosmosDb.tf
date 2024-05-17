3a31a4726a76f947267ce8e0475c52fa360f531e
Felipe Fabricio <ff.oliveira32@gmail.com>
2024-05-17 14:15:24 -0300

    Aplica correçõesm terraform fmt (#31)

--- 7 - CosmosDb.tf
+++ 7 - CosmosDb.tf
@@ -1,15 +1,15 @@
 resource "azurerm_cosmosdb_account" "wonderfood-cosmos-account" {
-  name                = "wonderfood-cosmosdb"
-  location            = azurerm_resource_group.wonderfood-rg.location
-  resource_group_name = azurerm_resource_group.wonderfood-rg.name
-  offer_type          = "Standard"
-  kind                = "MongoDB"
-  enable_free_tier    = true
+  name                              = "wonderfood-cosmosdb"
+  location                          = azurerm_resource_group.wonderfood-rg.location
+  resource_group_name               = azurerm_resource_group.wonderfood-rg.name
+  offer_type                        = "Standard"
+  kind                              = "MongoDB"
+  enable_free_tier                  = true
   is_virtual_network_filter_enabled = false
   enable_multiple_write_locations   = false
   enable_automatic_failover         = false
-  public_network_access_enabled = true
- 
+  public_network_access_enabled     = true
+
   consistency_policy {
     consistency_level = "Session"
   }
--- 8 - APIM.tf
+++ 8 - APIM.tf
@@ -1,10 +1,8 @@
-﻿resource "azurerm_api_management" "wonderfood-apim" {
+resource "azurerm_api_management" "wonderfood-apim" {
   name                = "wonderfood-apim"
   location            = azurerm_resource_group.wonderfood-rg.location
   resource_group_name = azurerm_resource_group.wonderfood-rg.name
   publisher_name      = "Wonderfood"
-publisher_email     = "felipefmo@outlook.com"
-
-
+  publisher_email     = "felipefmo@outlook.com"
   sku_name = "Developer_1"
 }
