414865fa5dd22bffbd93fa82517aac8f5d223af3
Felipe Fabricio <ff.oliveira32@gmail.com>
2024-05-17 14:12:21 -0300

    Alterações Terraform (#30)
    * Altera nomes e adiciona CosmosDB
    
    * Alterações terraform

---
+++ 7 - CosmosDb.tf
@@ -0,0 +1,34 @@
+resource "azurerm_cosmosdb_account" "wonderfood-cosmos-account" {
+  name                = "wonderfood-cosmosdb"
+  location            = azurerm_resource_group.wonderfood-rg.location
+  resource_group_name = azurerm_resource_group.wonderfood-rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+  enable_free_tier    = true
+  is_virtual_network_filter_enabled = false
+  enable_multiple_write_locations   = false
+  enable_automatic_failover         = false
+  public_network_access_enabled = true
+ 
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+
+  geo_location {
+    location          = azurerm_resource_group.wonderfood-rg.location
+    failover_priority = 0
+  }
+}
+
+
+resource "azurerm_cosmosdb_mongo_database" "wonderfood-mongo-db" {
+  name                = "Wonderfood"
+  resource_group_name = azurerm_resource_group.wonderfood-rg.name
+  account_name        = azurerm_cosmosdb_account.wonderfood-cosmos-account.name
+}
---
+++ 8 - APIM.tf
@@ -0,0 +1,10 @@
+﻿resource "azurerm_api_management" "wonderfood-apim" {
+  name                = "wonderfood-apim"
+  location            = azurerm_resource_group.wonderfood-rg.location
+  resource_group_name = azurerm_resource_group.wonderfood-rg.name
+  publisher_name      = "Wonderfood"
+publisher_email     = "felipefmo@outlook.com"
+
+
+  sku_name = "Developer_1"
+}
